b3144932b2882d82b65036e0476eddb5bdd952ef
parkbyunggwon <startartart@naver.com>
2023-01-11 20:25:29 +0900

    FEAT: 컴포넌트 추가 및 이벤트 제어

--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled, { css } from 'styled-components';
-import { MdDone, MdDelete } from 'react-icons/md';
+import { MdDone, MdDelete, MdOutlinePublish } from 'react-icons/md';
 import { useTodoDispatch } from '../TodoContext';
 
 const Remove = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #dee2e6;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+  display: none;
+`;
+
+const Change = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -22,9 +35,20 @@ const TodoItemBlock = styled.div`
   padding-top: 12px;
   padding-bottom: 12px;
   &:hover {
-    ${Remove} {
-      display: initial;
-    }
+    ${props =>
+      !props.editable &&
+      css`
+        ${Remove} {
+          display: initial;
+        }
+      `}
+    ${props =>
+      props.editable &&
+      css`
+        ${Change} {
+          display: initial;
+        }
+      `}
   }
 `;
 
@@ -46,31 +70,88 @@ const CheckCircle = styled.div`
       color: #38d9a9;
     `}
 `;
-
+const Input = styled.input`
+  flex: 1;
+  display: block;
+  font-size: 23px;
+  color: #495057;
+  border: none;
+  outline: none;
+  border-radius: 2rem;
+  background-color: #f5fffa;
+  ${props =>
+    !props.editable &&
+    css`
+      display: none;
+    `}
+`;
+    
 const Text = styled.div`
   flex: 1;
   font-size: 21px;
   color: #495057;
+  border: none;
+  outline: none;
   ${props =>
     props.done &&
     css`
       color: #ced4da;
     `}
+  ${props =>
+    props.editable &&
+    css`
+      display: none;
+    `}
+  &:hover {
+    color: #ff6b6b;
+  }
 `;
 
-function TodoItem({ id, done, text }) {
+const InputForm = styled.form`
+`;
+
+function TodoItem({ id, done, text, editable }) {
   const dispatch = useTodoDispatch();
+
+  const [value, setValue] = useState(text);
+
   const onToggle = () => dispatch({type: "TOGGLE", id});
   const onRemove = () => dispatch({type: "REMOVE", id});
+  const onEdit = () => dispatch({type: "EDIT", id});
+  const onChange = e => setValue(e.target.value);
+  const onSubmit = e => {
+    e.preventDefault(); // 새로고침 방지
+    dispatch({
+      type: "UPDATE",
+      id,
+      text: value
+    });
+    dispatch({
+      type: "EDIT",
+      id
+    });
+  };
+
   return (
-    <TodoItemBlock>
+    <TodoItemBlock editable={editable}>
       <CheckCircle done={done} onClick={onToggle}>
         {done && <MdDone />}
       </CheckCircle>
-      <Text done={done}>{text}</Text>
+      <Text done={done} onClick={onEdit} editable={editable}>{text}</Text>
+      <InputForm onSubmit={onSubmit}>
+        <Input 
+          value={value} 
+          editable={editable}
+          autoFocus
+          onChange={onChange}
+        />
+      </InputForm>
       <Remove onClick={onRemove}>
         <MdDelete />
       </Remove>
+      <Change onClick={onSubmit}>
+        <MdOutlinePublish />
+      </Change>
     </TodoItemBlock>
   );
 }
